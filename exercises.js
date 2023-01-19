fd9840f8d919e9a5b61abf608373a12e9c6a0846
Celybot <youancece@gmail.com>
2023-01-19 06:50:38 -0500

    solved questions

--- exercises.js
+++ exercises.js
@@ -1,38 +1,115 @@
 // Question 1
-const isUpperCase = () => {
+const isUpperCase = (str) => {
+let upperCaseStr = str.toUpperCase()
+if(upperCaseStr === str){
+  return true;
+}else{
+  return false
+}
 
 };
+// console.log(isUpperCase('t'))            // false
+// console.log(isUpperCase('T'))             // true
+// console.log(isUpperCase('Four Score'))  ;      // false
+// console.log(isUpperCase('FOUR SCORE'))  ;      // true
+// console.log(isUpperCase('4SCORE!'))  ;         // true
+// console.log(isUpperCase(''))  ; 
 
 // Question 2
-const removeVowels = () => {
-
+ const removeVowels = (arr) => {
+  return arr.map(value => value.replace(/[AEIOU]/gi,""))
+  // let str = "A,E,I,O,U,a,e,i,o,u"
+  // let newArr = []
+  // for(let i = 0; i < arr.length; i++){
+  //   let newStr = ""
+  //   for(let j = 0; j < arr[i].length; j++){
+  //     if(!str.includes(arr[i][j])){
+  //       newStr+= arr[i][j]
+  //     }
+  //   }
+  //   newArr.push(newStr)
+  // }
+  // console.log(newArr)
 };
+// console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]  // ["grn", "YLLW", "blck", "wht"]
+// console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+// console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
 
 // Question 3
-const wordCap = () => {
+const wordCap = (str) => {
+  return str.toLowerCase().split(" ").map((element) => element[0].toUpperCase() + element.substring(1)).join(" ")
+}
 
-};
+console.log(wordCap('the javaScript language'));     
+console.log(wordCap('four score and seven'));
+console.log(wordCap('this is a "quoted" word'));
 
 // Question 4
-const swapCase = () => {
-
+const swapCase = (str) => {
+  let newStr = ""
+  //str.split().forEach(element => {element.toLowerCase() !== element ? newStr += element.toLowerCase() : newStr += element.toUpperCase()})
+  
+  for(i in str){
+    if (str[i].toLowerCase() !== str[i]){
+        newStr += str[i].toLowerCase()
+    }else{
+      newStr += str[i].toUpperCase()
+    }
+  }
+  return newStr
 };
+// console.log(swapCase('CamelCase'));              // "cAMELcASE"
+// console.log(swapCase('Tonight on XYZ-TV'));      // "tONIGHT ON xyz-tv"
 
 // Question 5
-const staggeredCase = () => {
-
-};
+const staggeredCase = (str) => {
+  // let regex = [a-zA-Z]
+  // let newStr = str[0].toUpperCase()
+  // for(let i =0; i<str.length; i++){
+  //   if(str[i].match(regex)){
+  //     console.log(str[i])
+  //   }
+  // }
+}
+console.log(staggeredCase('ignore 77 the 444 numbers'))
 
 // Question 6
-const wordLengths = () => {
-
+const wordLengths = (str) => {
+  let newArr = []
+  str = str.split(" ")
+ for(element of str){
+  console.log(element)
+  newArr.push(element + ` ${element.length}`)
+ }
+ return newArr
 };
+// console.log(wordLengths('cow sheep chicken'));
+// // ["cow 3", "sheep 5", "chicken 7"]
 
+// console.log(wordLengths('baseball hot dogs and apple pie'));
+// // ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+// console.log(wordLengths("It ain't easy, is it?"));
+// // ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+// console.log(wordLengths('Supercalifragilisticexpialidocious'));
+// // ["Supercalifragilisticexpialidocious 34"]
 // Question 7
-const searchWord = () => {
-
+let text = 'D Smoke is humble. The Inglewood native exudes an aura of maturation, needed for his quick ascension into popular culture as the first winner of Rhythm + Flow , Netflix’s hip-hop reality competition centered on the discovery of hip-hop’s next star. His signature authenticity shone throughout the 10-episode series and international audiences were drawn to his charisma as he proudly rapped about his lived experiences as a young black man in Inglewood.';
+const searchWord = (word,text) => {
+  let counter = 0
+  //text = text.toLowerCase().split(' ')
+  for(let i =0 ; i<text.length; i++){
+    if(text[i] === word){
+      counter++
+    }
+  }
+  return counter
 };
-
+console.log(searchWord('his', text));      // 4 
+console.log(searchWord('hip-hop', text));  // 1 (because "hip-hop's" !== "hip-hop")
+//console.log(searchWord('flow', text));           // 1
+//console.log(searchWord('is', text));           // 1
 
 // The code below ensures that this file can talk to our test file.
 module.exports = {
--- exercises.test.js
+++ exercises.test.js
@@ -1,3 +1,4 @@
+const { TestScheduler } = require('jest');
 const exercises = require('./exercises');
 
 //write tests in this file
@@ -8,3 +9,42 @@ test('Question 0: add', () => {
   expect(exercises.add(1, -1)).toEqual(0);
 });
 */
+test('isUpperCase function', () => {
+  expect(exercises.isUpperCase('t')).toEqual(false);
+  expect(exercises.isUpperCase('T')).toEqual(true);
+  expect(exercises.isUpperCase('Four Score')).toEqual(false);
+  expect(exercises.isUpperCase('FOUR SCORE')).toEqual(true);
+  expect(exercises.isUpperCase('')).toEqual(true);
+});
+test('remove vowel function', () => {
+  expect(exercises.removeVowels(['abcdefghijklmnopqrstuvwxyz'])).toEqual(["bcdfghjklmnpqrstvwxyz"]);
+  expect(exercises.removeVowels(['green', 'YELLOW', 'black', 'white'])).toEqual(["grn", "YLLW", "blck", "wht"]);
+  expect(exercises.removeVowels(['ABC', 'AEIOU', 'XYZ'])).toEqual(["BC", "", "XYZ"]);
+});
+test('word cap function', () => {
+  expect(exercises.wordCap('four score and seven')).toEqual("Four Score And Seven");
+  expect(exercises.wordCap('the javaScript language')).toEqual("The Javascript Language");
+  expect(exercises.wordCap('this is a "quoted" word')).toEqual('This Is A "quoted" Word');
+});
+test('swapCase function', ()=> {
+  expect(exercises.swapCase('CamelCase')).toEqual("cAMELcASE")
+  expect(exercises.swapCase('Tonight on XYZ-TV')).toEqual("tONIGHT ON xyz-tv")
+});
+
+test('staggeredCase function',()=> {
+  expect(exercises.staggeredCase('I Love Launch School!')).toEqual("I lOvE lAuNcH sChOoL!")
+  expect(exercises.staggeredCase("ALL CAPS")).toEqual("AlL cApS")
+  expect(exercises.staggeredCase('ignore 77 the 444 numbers')).toEqual("IgNoRe 77 ThE 444 nUmBeRs")
+})
+test('wordLengths functiom',()=> {
+  expect(exercises.wordLengths('cow sheep chicken')).toEqual(["cow 3", "sheep 5", "chicken 7"])
+  expect(exercises.wordLengths('baseball hot dogs and apple pie')).toEqual(["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"])
+  expect(exercises.wordLengths("It ain't easy, is it?")).toEqual(["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"])
+  expect(exercises.wordLengths("Supercalifragilisticexpialidocious")).toEqual(["Supercalifragilisticexpialidocious 34"])
+})
+test("searchWord function", () => {
+  expect(exercises.searchWord('his')).toEqual(4);
+  expect(exercises.searchWord('hip-hop')).toEqual(1); 
+  expect(exercises.searchWord('flow')).toEqual(1); 
+  expect(exercises.searchWord('is')).toEqual(1); 
+})
